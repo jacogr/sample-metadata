b028c0cf6c8c3512a16e07193a28f8bcc92efae5
Jaco Greeff <jacogr@gmail.com>
2020-12-21 13:58:22 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var metadata_1 = require("@polkadot/metadata");
+var types_1 = require("@polkadot/types");
+var registry = new types_1.TypeRegistry();
+var metadata = new metadata_1.Metadata(registry);
---
+++ index.ts
@@ -0,0 +1,5 @@
+import { Metadata } from '@polkadot/metadata';
+import { TypeRegistry } from '@polkadot/types';
+
+const registry = new TypeRegistry();
+const metadata = new Metadata(registry);
